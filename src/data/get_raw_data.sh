721d58a386559d6889d68fd09c556d6f9f07c779
Lara <gtlchaves@gmail.com>
2022-05-02 12:51:06 -0300

    Altera caminho para raw data

--- src/data/get_raw_data.sh
+++ src/data/get_raw_data.sh
@@ -2,4 +2,4 @@
 
 echo Certifique que zenodo_get está instalado no ambiente atual
 
-zenodo_get -t 45 -p 0.5 -o ${1-../../data/raw/zenodo_data} 1321278
+zenodo_get -t 45 -p 0.5 -o ${1-../../data/raw/} 1321278
