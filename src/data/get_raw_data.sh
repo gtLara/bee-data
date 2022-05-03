a9a2cbc019261598a6242c24efd4c4229b316ce1
Lara <gtlchaves@gmail.com>
2022-05-03 01:40:08 -0300

    Adiciona script de segmentação

--- src/data/get_raw_data.sh
+++ src/data/get_raw_data.sh
@@ -2,6 +2,10 @@
 
 echo Certifique que zenodo_get está instalado no ambiente atual
 
-zenodo_get -t 45 -p 0.5 -R 10 -o ${1-../../data/raw/} 1321278
-zenodo_get -t 45 -p 0.5 -R 10 -o ${1-../../data/raw/} 1321278
-zenodo_get -t 45 -p 0.5 -R 10 -o ${1-../../data/raw/} 1321278
+# Múltiplas iterações para garantir download
+# TODO: explicar
+
+for i in {0..2}
+do
+    zenodo_get -t 45 -p 0.5 -R 10 -o ${1-../../data/raw/} 1321278
+done
