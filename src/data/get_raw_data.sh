013d49d5261ca8ed58732f895747cd26cbee60b2
Lara <gtlchaves@gmail.com>
2022-05-02 22:48:46 -0300

    Adiciona parâmetro para repetição de download em até 10 falhas

--- src/data/get_raw_data.sh
+++ src/data/get_raw_data.sh
@@ -2,4 +2,4 @@
 
 echo Certifique que zenodo_get está instalado no ambiente atual
 
-zenodo_get -t 45 -p 0.5 -o ${1-../../data/raw/} 1321278
+zenodo_get -t 45 -p 0.5 -R 10 -o ${1-../../data/raw/} 1321278
