e5a676181d5bf46a28699eb614144a00599a376a
Lara <gtlchaves@gmail.com>
2022-05-02 22:49:17 -0300

    Repete operação de download duas vezes para garantir completude

--- src/data/get_raw_data.sh
+++ src/data/get_raw_data.sh
@@ -3,3 +3,5 @@
 echo Certifique que zenodo_get está instalado no ambiente atual
 
 zenodo_get -t 45 -p 0.5 -R 10 -o ${1-../../data/raw/} 1321278
+zenodo_get -t 45 -p 0.5 -R 10 -o ${1-../../data/raw/} 1321278
+zenodo_get -t 45 -p 0.5 -R 10 -o ${1-../../data/raw/} 1321278
